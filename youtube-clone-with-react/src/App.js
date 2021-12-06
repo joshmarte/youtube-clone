46294b2a816fdf897c0e5ad59a485fbd11b03269
Theodore <theo.frazier@icloud.com>
2021-12-06 15:07:56 -0500

    got video to appear using API

--- youtube-clone-with-react/src/App.js
+++ youtube-clone-with-react/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import YouTube from 'react-youtube'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      Hi Guys!
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movieList: [],
+    }
+  }
+
+
+
+  render() {
+    const opts = {
+      height: '300',
+      width: '600',
+      playerVars: {
+        autoplay: 1,
+      }
+    }
+
+    return (
+      <iframe id="ytplayer" type="text/html" width="640" height="360"
+      src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+      frameborder="0"></iframe>
+    );
+
+
+
+  }
 }
 
 export default App;
