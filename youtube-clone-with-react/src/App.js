d9947fe620eb650b1bce9ebd3a71c230b337d427
JoshMarte <joshuelm@gmail.com>
2021-12-07 12:41:14 -0500

    working api call and results component

--- youtube-clone-with-react/src/App.js
+++ youtube-clone-with-react/src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Results from "./components/Results";
 
-function App() {
-  return (
-    <div>
-      Hi Guys!
-    </div>
-  );
+require("dotenv").config();
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: "",
+      apiResults: [],
+      results: false,
+    };
+  }
+
+  fetchData = async () => {
+    const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+    let res = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${this.state.searchTerm}&type=video&key=${API_KEY}`
+    );
+    let data = await res.json();
+    this.setState({
+      // searchTerm: "",
+      apiResults: data.items,
+      results: true,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        Hi Guys!
+        <Results results={this.state.apiResults} />
+      </div>
+    );
+  }
 }
 
 export default App;
